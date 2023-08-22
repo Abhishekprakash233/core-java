82af8ee436241598f94531e87e6e36a210145309
Abhishekprakash233 <abhishekprakash.xworkz@gmail.com>
2023-08-22 12:32:52 +0530

    Array files

---
+++ OttPlatformNew.java
@@ -0,0 +1,19 @@
+class OttPlatformNew{
+static String englishMovies[] = {"The Shawshank Redemption", "The Godfather", "The Dark Knight", "The Godfather Part", "Angry Men"};
+static String HindiSeries[]={"Mirzapur", "Flames","Delhi Crime","Garmi" "Rudra","Asur", "Roar","Panchayut","Kohra","Gullak"};
+static String TeluguSeries[]={"Parampara", "Locked", "Vyavastha","Rafuchakkar", "Vilangu","Hawar","The Great Indian Murder"};
+static String KannadaMovies[]={"Ambuja","Damyana","Aparoopa","Savitri","Porki","Rathaavara","Parava","Chalo","Kirathaka"};
+public static void main(String movies[]){
+   System.out.println("Main started");
+   System.out.println("The Collection of Movies are:");
+   System.out.println("englishMovies.length");
+System.out.println(englishMovies[15]);   
+   
+   //for              expression(realatonal operation)
+    //for                      0 , 12
+   for(int abhi=0; abhi<englishMovies.length; abhi++){
+   System.out.println(englishMovies[abhi]);
+   }
+   System.out.println("Main ended");
+   }
+   }V
